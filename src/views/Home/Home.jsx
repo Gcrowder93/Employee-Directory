b779b83bd19f654cd03b358f418f4269765a8751
Chase Crowder <gcrowder93@gmail.com>
2022-02-24 10:23:23 -0800

    i think im at a starting point, starting to route

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,17 +1,17 @@
-// import { Link, Redirect } from 'react-router-dom';
-// import { useUser } from '../../context/UserContext';
+import { Link, Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext';
 
-// export default function Home() {
-//   const auth = useUser();
-//   if (auth.user.email) return <Redirect to="/notes" />;
+export default function Home() {
+  const auth = useUser();
+  if (auth.user.email) return <Redirect to="/notes" />;
 
-//   return (
-//     <section>
-//       <h1>Welcome</h1>
-//       <p>
-//         Please <Link to="/login">sign in</Link> or
-//         <Link to="/register">register.</Link>
-//       </p>
-//     </section>
-//   );
-// }
+  return (
+    <section>
+      <h1>Welcome</h1>
+      <p>
+        Please <Link to="/login">sign in</Link> or
+        <Link to="/register">register.</Link>
+      </p>
+    </section>
+  );
+}
