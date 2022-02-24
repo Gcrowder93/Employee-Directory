fbe5d626e3aacead8201a776ba4b4e1d62f604ce
Chase Crowder <gcrowder93@gmail.com>
2022-02-24 11:24:05 -0800

    added a header, and some instructions for myself

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
-import './Header.css';
+import styles from './Header.css';
 
 export default function Header() {
   return (
     <>
-      <header>
+      <header className={styles.header}>
         <Link to="/">
-          <h1>Home Page (this is a header)</h1>
+          <h3>Home Page (this is a header)</h3>
         </Link>
+        <h5>
+          need Sign in button if user is signed out<br></br>
+          and 'you are signed in as ($)email with a sign out button if user is
+          signed in
+        </h5>
       </header>
     </>
   );
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -8,7 +8,11 @@ function Auth() {
       <h3>
         this page is the log in page.<br></br>
         should have form with should have 2 inputs (email, pass)<br></br> and 2
-        buttons (login, register)
+        buttons (login, register)<br></br>
+        register button takes you to /register<br></br>
+        <br></br>
+        login button takes you to /home, with protected links to edit profile{' '}
+        <br></br>OR to an edit profile page with inputs for birthday and bio
       </h3>
       <Link to="/">Back Home</Link>
     </>
