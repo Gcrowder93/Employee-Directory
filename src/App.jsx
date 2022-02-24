7617669edbe430e885c488ff82989323dac5baa0
Chase Crowder <gcrowder93@gmail.com>
2022-02-23 17:24:10 -0800

    laughter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import './App.css';
+import image from './assets/huh.png';
+import React from 'react';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello beautiful World</h1>
+      <h3>and here we go</h3>
+      <img src={image} height={320} width={320}></img>
+    </>
+  );
 }
