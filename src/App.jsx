622d9fe6eb4b06b3fe7a13b8e626d35156d2650e
Chase Crowder <gcrowder93@gmail.com>
2022-02-24 10:21:14 -0800

    making sure things work

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <h1>Hello beautiful World</h1>
+      <h1>Hell World</h1>
       <h3>and here we go</h3>
       <img src={image} height={320} width={320}></img>
     </>
